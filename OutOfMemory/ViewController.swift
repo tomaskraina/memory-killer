96ae3d67ec02f0ada7509a98c0b38e927de5d3c1
Tom Kraina <tomas.kraina@gmail.com>
2015-10-01 15:59:12 +0200

    Initial implementation.

--- OutOfMemory/ViewController.swift
+++ OutOfMemory/ViewController.swift
@@ -15,11 +15,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let mutableString = NSMutableString()
+        while true {
+            let string = NSUUID().UUIDString
+            mutableString.appendString(string)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        print("didReceiveMemoryWarning")
     }
 
 
 }
-
